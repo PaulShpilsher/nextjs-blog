6438e72edca0d4c81b97b1495024dd1d5e50bb76
Paul Shpilsher <paul.shpilsher@gmail.com>
2023-11-30 16:50:11 +0300

    add Post page

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,7 +1,16 @@
-import Layout from '../../components/layout';
+import Layout from "../../components/layout";
+import { getPostData } from "../../lib/posts";
 
-export default function Post() {
-  return <Layout>...</Layout>;
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  );
 }
 
 export async function getStaticPaths() {
@@ -15,4 +24,10 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
   // Fetch necessary data for the blog post using params.id
+  const postData = getPostData(params.id);
+  return {
+    props: {
+      postData: postData,
+    },
+  };
 }
