715806e900dc0aeeef4308a6417a163b25ae509d
Paul Shpilsher <paul.shpilsher@gmail.com>
2023-11-30 17:10:22 +0300

    add Date component

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,3 +1,4 @@
+import Date from "../../components/date";
 import Layout from "../../components/layout";
 import { getPostData, getAllPostIds } from "../../lib/posts";
 import Head from 'next/head';
@@ -10,7 +11,7 @@ export default function Post({ postData }) {
       </Head>
       {postData.id}
       <br />
-      {postData.date}
+      <Date dateString={postData.date} />
       <br />
       <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
