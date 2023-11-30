a0d581d44d6f0518c4ef437e95c304e848b00706
Paul Shpilsher <paul.shpilsher@gmail.com>
2023-11-30 18:35:22 +0300

    add param info

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,9 @@
+
+/**
+ * 
+ * @param {http.IncomingMessage} req @see https://nodejs.org/api/http.html#http_class_http_incomingmessage
+ * @param {http.ServerResponse} res @see https://nodejs.org/api/http.html#http_class_http_serverresponse
+ */
 export default function handler(req, res) {
   res.status(200).json({ text: 'Hello' });
 }
