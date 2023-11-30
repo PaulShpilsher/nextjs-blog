4c777e6401bca3ad3b34d80102901b2a2ed6592d
Paul Shpilsher <paul.shpilsher@gmail.com>
2023-11-30 16:35:07 +0300

    implement getStaticPaths

--- components/layout.jsx
+++ components/layout.jsx
@@ -5,7 +5,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 
 
-const name = 'Pasha';
+const name = 'Paul';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
